77627db89faccc7c64e608a749602be0edaecb2e
Mone't Fulgham <monetfulgham@gmail.com>
2018-01-08 14:05:36 -0500

    Updated test code and main module

--- server.js
+++ server.js
@@ -4,3 +4,16 @@ const app = express();
 const morgan = require('morgan');
 app.use(express.static('public'));
 app.listen(process.env.PORT || 8080);
+
+
+
+
+
+
+if (require.main === module) {
+    app.listen(process.env.PORT || 8080, function () {
+        console.info(`App listening on ${this.address().port}`);
+    });
+}
+
+module.exports = app;
--- test/test-server.js
+++ test/test-server.js
@@ -4,7 +4,7 @@ const chai= require('chai');
 const chaiHttp= require ('chai-http');
 chai.should();
 chai.use(chaiHttp);
-const app= require('../server');
+const app= require('../server.js');
 
 describe ('Index page', function(){
     it ('the index page HTML shows up', function(){
