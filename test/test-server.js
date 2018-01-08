e5d2237e4608061a47c1c8b578e402faf9e6ee74
Mone't Fulgham <monetfulgham@gmail.com>
2018-01-08 13:45:31 -0500

    Add test folder and initial file

---
+++ test/test-server.js
@@ -0,0 +1,17 @@
+const express= require('express');
+const mocha= require('mocha');
+const chai= require('chai');
+const chaiHttp= require ('chai-http');
+chai.should();
+chai.use(chaiHttp);
+const app= require('../server');
+
+describe ('Index page', function(){
+    it ('the index page HTML shows up', function(){
+        return chai.request(app)
+        .get('/')
+        .then(function(response){
+            response.should.have.status(200);
+        });
+    });
+});
