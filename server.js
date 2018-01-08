d59088ef8279143aacc77925e056b5edd4a2e2be
Mone't Fulgham <monetfulgham@gmail.com>
2018-01-08 13:27:18 -0500

    Updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const mocha = require('mocha');
 const app = express();
 const morgan = require('morgan');
 app.use(express.static('public'));
