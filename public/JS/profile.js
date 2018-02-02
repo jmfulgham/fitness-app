9c6085d1241871138a71fa43a32fac9c60002152
Mone't Fulgham <monetfulgham@gmail.com>
2018-02-02 18:19:24 -0500

    Separate API and Routine functions

--- public/JS/profile.js
+++ public/JS/profile.js
@@ -1,29 +1,112 @@
-//pull the username from /profile/name/:username
-//sent the username to my API request
-//create API request for profile/JSON/:username
-//return API data to front end
-//separate data by date to front end
-//create small cards with each workout listed by date
+'use strict';
+
+let workoutDate = "";
+let upperWorkout = "";
+let lowerWorkout = "";
 
 
+function showOneDate(date) {
 
-function getProfileName(username){
-    $.ajax({
+    return `<h3>${date}</h3>`;
+}
+
+
+function showOneUpperWorkout(upper) {
+    let upperWorkout = "";
+
+    if (upper === undefined) {
+        upperWorkout = upperWorkout + "<h4> No upper body workout </h4>";
+    } else {
+        upperWorkout += `
+            <section class="upper">
+            <h4>Upper</h4>
+            <h5>${upper.Exercise}</h5>
+            <ul>
+                <li>Sets: ${upper.Sets}</li>
+                <li>Reps: ${upper.Reps}</li>
+                <li>Lbs: ${upper.Lbs}</li>
+            </ul>
+        </section>`;
+    }
+
+    return upperWorkout;
+}
+
+function showAllUpperWorkout(uppers) {
+    let result = ''
+    uppers.forEach(function (item) {
+        result = result + showOneUpperWorkout(item);
+    })
+    return result;
+}
+
+
+function showOneLowerWorkout(lower) {
+    let lowerWorkout = "";
+
+    if (lower == undefined) {
+        lowerWorkout = lowerWorkout + "<h4> No lower body workout </h4>";
+    } else {
+        lowerWorkout += `
+      <section class="lower">
+        <h4>Lower</h4>
+           <h5>${lower.Exercise}</h5>
+           <ul>
+             <li>Sets: ${lower.Sets}</li>
+             <li>Reps: ${lower.Reps}</li>
+             <li>Lbs: ${lower.Lbs}</li>
+           </ul>
+      </section>`;
+    }
+    return lowerWorkout;
+
+}
+
+function showAllLowerWorkout(lowers) {
+    let result = ''
+    lowers.forEach(function (item) {
+        result = result + showOneLowerWorkout(item);
+    })
+
+    return result;
+}
+
+function addEditButton(){
+    let button = `<button type="button" value="edit" id="edit">Edit</button>`;
+    return button ;
+
+}
+
+function displayOriginalObject(originalObject) {
+    originalObject.forEach(function (item) {
+        let htmlDate = showOneDate(item.date);
+        let htmlUpperWorkout = showAllUpperWorkout(item.upper);
+        let htmlLowerWorkout = showAllLowerWorkout(item.lower);
+        // let buttons = addEditButton();
+        // console.log(buttons);
+        let result = htmlDate + htmlUpperWorkout + htmlLowerWorkout;
+        $('.list').append(`<section class="col-4">${result}<button type="button" value="edit" id="edit">Edit</button>
+        <button type="button" value="delete" id="delete">Delete</button></section>`);
+    })
+
+}
+
+function getProfile(username) {
+    $ajax({
         type: "GET",
-        url: `http://localhost:9000/profile/name/${username}`,
+        url: `localhost:9000/profile/names/${username}`,
         dataType: 'json',
         headers: {
             "Content-Type": "application/json"
         },
-        success: function (theName) {
-            console.log("the name", theName);
+        success: function (list) {
+            return list
         }
     })
 }
 
 
-function getProfile(username) {
-    
+function getJSONProfile(username) {
     $.ajax({
         type: "GET",
         url: `http://localhost:9000/profile/JSON/${username}`,
@@ -31,41 +114,8 @@ function getProfile(username) {
         headers: {
             "Content-Type": "application/json"
         },
-        success: function (secondList) {
-            console.log(secondList);
+        success: function (profilesList) {
+            displayOriginalObject(profilesList);
         }
-    })
-}
-
-function createSections(fullWorkout){
-    console.log("full ", fullWorkout);
-    $('.list').append(`<div class="full"><h3>${fullWorkout.date}</h3><section class="upper"></section><section class=
-    "lower"></section</div>`)
-}
-
-
-function showUpperWorkout(upper) {
-    console.log(upper);
-    upper.forEach(function (upperBody) {
-        createSections(upperBody);
-        $(".upper").append();
-        if (upperBody === undefined) {
-            return $(".upper").append("<h4> No upper body workout </h4>");
-        }
-        $(".upper").append(`<h5>Upper Body</h5><ul>${upperBody.Exercise}</ul><li>Sets: ${upperBody.Sets}</li>
-            <li>Reps: ${upperBody.Reps}</li><li>Lbs: ${upperBody.Lbs}</li>`);
-    })
-}
-
-function showLowerWorkout(workouts) {
-    let lower = workouts.lower;
-    console.log("Lower ", lower);
-    $(".workout").append(`<h5>Lower Body<h5>`);
-    lower.forEach(function (lowerBody) {
-        if (lowerBody === undefined) {
-            return $(".lower").append("<h4> No lower body workout </h4>")
-        }
-        $(".workout").append(`<ul>${lowerBody.Exercise}</ul><li>Sets: ${lowerBody.Sets}</li>
-            <li>Reps: ${lowerBody.Reps}</li><li>Lbs: ${lowerBody.Lbs}</li>`);
     })
 }
--- public/JS/routineList.js
+++ public/JS/routineList.js
@@ -1,8 +1,6 @@
 'use strict';
 
-let workoutDate = "";
-let upperWorkout = "";
-let lowerWorkout = "";
+
 
 function getAllRoutines() {
     $.ajax({
@@ -18,34 +16,7 @@ function getAllRoutines() {
     })
 }
 
-function getProfile(username) {
-    $ajax({
-        type: "GET",
-        url: `localhost:9000/profile/names/${username}`,
-        dataType: 'json',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        success: function (list) {
-            return list
-        }
-    })
-}
 
-
-function getJSONProfile(username) {
-    $.ajax({
-        type: "GET",
-        url: `http://localhost:9000/profile/JSON/${username}`,
-        dataType: 'json',
-        headers: {
-            "Content-Type": "application/json"
-        },
-        success: function (profilesList) {
-            displayOriginalObject(profilesList);
-        }
-    })
-}
 
 //routines API
 function handleNames(list) {
@@ -73,86 +44,4 @@ function removeDupes(list) {
 }
 
 
-function showOneDate(date) {
-    
-    return `<h3>${date}</h3>`;
-}
-
-
-function showOneUpperWorkout(upper) {
-    let upperWorkout = "";
-
-    if (upper === undefined) {
-        upperWorkout = upperWorkout + "<h4> No upper body workout </h4>";
-    } else {
-
-        upperWorkout += `
-      <section class="upper">
-        <h4>Upper</h4>
-           <h5>${upper.Exercise}</h5>
-           <ul>
-             <li>Sets: ${upper.Sets}</li>
-             <li>Reps: ${upper.Reps}</li>
-             <li>Lbs: ${upper.Lbs}</li>
-           </ul>
-      </section>`;
-    }
-
-    return upperWorkout;
-}
-
-function showAllUpperWorkout(uppers) {
-    let result = ''
-    uppers.forEach(function (item) {
-        result = result + showOneUpperWorkout(item); 
-    })
-
-    return result;
-}
-
-
-function showOneLowerWorkout(lower) {
-    let lowerWorkout = "";
-
-    if (lower == undefined) {
-        lowerWorkout = lowerWorkout + "<h4> No lower body workout </h4>";
-    } else {
-
-        lowerWorkout += `
-      <section class="lower">
-        <h4>Lower</h4>
-           <h5>${lower.Exercise}</h5>
-           <ul>
-             <li>Sets: ${lower.Sets}</li>
-             <li>Reps: ${lower.Reps}</li>
-             <li>Lbs: ${lower.Lbs}</li>
-           </ul>
-      </section>`;
-    }
-
-
-    return lowerWorkout;
-
-}
-
-function showAllLowerWorkout(lowers) {
-    let result = ''
-    lowers.forEach(function (item) {
-        result = result + showOneLowerWorkout(item); 
-    })
-
-    return result;
-    //checked
-}
-
-function displayOriginalObject(originalObject) {
-    originalObject.forEach(function (item) {
-        let htmlDate = showOneDate(item.date); 
-        let htmlUpperWorkout = showAllUpperWorkout(item.upper);
-        let htmlLowerWorkout = showAllLowerWorkout(item.lower);
-        let result = htmlDate + htmlUpperWorkout + htmlLowerWorkout;
-        $('.list').append(`<section class="col-4">${result}</section>`);
-    })
-
-}
 
