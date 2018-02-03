954705759f575f93e1a98d31e82fcb225a12a70a
Mone't Fulgham <monetfulgham@gmail.com>
2018-02-03 15:49:31 -0500

    Editing FE for initial PUT request

--- public/JS/profile.js
+++ public/JS/profile.js
@@ -11,20 +11,19 @@ function showOneDate(date) {
 }
 
 
-function showOneUpperWorkout(upper) {
+function showOneUpperWorkout(upper, setColumn) {
     let upperWorkout = "";
-
     if (upper === undefined) {
         upperWorkout = upperWorkout + "<h4> No upper body workout </h4>";
     } else {
         upperWorkout += `
             <section class="upper">
             <h4>Upper</h4>
-            <h5>${upper.Exercise}</h5>
+            <h5class="${setColumn}">${upper.Exercise}</h5>
             <ul>
-                <li>Sets: ${upper.Sets}</li>
-                <li>Reps: ${upper.Reps}</li>
-                <li>Lbs: ${upper.Lbs}</li>
+                <li class="${setColumn}">Sets: ${upper.Sets}</li>
+                <li class="${setColumn}">Reps: ${upper.Reps}</li>
+                <li class="${setColumn}">Lbs: ${upper.Lbs}</li>
             </ul>
         </section>`;
     }
@@ -49,18 +48,21 @@ function showOneLowerWorkout(lower) {
     } else {
         lowerWorkout += `
       <section class="lower">
-        <h4>Lower</h4>
-           <h5>${lower.Exercise}</h5>
+        <h4 contenteditable="true">Lower</h4>
+           <h5 contenteditable="true">${lower.Exercise}</h5>
            <ul>
-             <li>Sets: ${lower.Sets}</li>
-             <li>Reps: ${lower.Reps}</li>
-             <li>Lbs: ${lower.Lbs}</li>
+             <li class="sets">Sets: ${lower.Sets}</li>
+             <li contenteditable="true">Reps: ${lower.Reps}</li>
+             <li contenteditable="true">Lbs: ${lower.Lbs}</li>
            </ul>
       </section>`;
     }
     return lowerWorkout;
 
 }
+
+
+
 
 function showAllLowerWorkout(lowers) {
     let result = ''
@@ -71,28 +73,59 @@ function showAllLowerWorkout(lowers) {
     return result;
 }
 
-function addEditButton(){
-    let button = `<button type="button" value="edit" id="edit">Edit</button>`;
-    return button ;
 
-}
 
 function displayOriginalObject(originalObject) {
-    originalObject.forEach(function (item) {
+    originalObject.forEach(function (item, index) {
         let htmlDate = showOneDate(item.date);
-        let htmlUpperWorkout = showAllUpperWorkout(item.upper);
+        let htmlUpperWorkout = showAllUpperWorkout(item.upper, `setColumn${index}`);
         let htmlLowerWorkout = showAllLowerWorkout(item.lower);
         // let buttons = addEditButton();
         // console.log(buttons);
         let result = htmlDate + htmlUpperWorkout + htmlLowerWorkout;
-        $('.list').append(`<section class="col-4">${result}<button type="button" value="edit" id="edit">Edit</button>
+        $('.list').append(`<section class="col-4">${result}<button type="button" value="edit" class="edit${index}">Edit</button>
         <button type="button" value="delete" id="delete">Delete</button></section>`);
+        handleEdit(`.edit${index}`, `.setColumn${index}`);
+    })
+    
+}
+
+
+// function handleDelete(id) {
+//     //retrieve the ID of this workout
+//     //send DELETE request
+//     //display "Delete Successful" message when done
+//     $ajax({
+//         type: "DELETE",
+//         url: `localhost:9000/workout/JSON/${id}`,
+//         dataType: 'json',
+//         headers: {
+//             "Content-Type": "application/json"
+//         },
+//         success: function (list) {
+//             return list
+//         }
+//     })
+// }
+
+function handleEdit(classButton, classSetColumn) {
+    console.log('edit')
+    $(classButton).on('click', event => {
+        console.log("clicked");
+        var isEditable = $(classSetColumn).is('.editable');
+        $(classSetColumn).prop('contenteditable', !isEditable);
+        $(classSetColumn).toggleClass('editable');
+        console.log(isEditable);
+        console.log(classSetColumn);
     })
 
 }
 
+// $(handleEdit())
+
+
 function getProfile(username) {
-    $ajax({
+    $.ajax({
         type: "GET",
         url: `localhost:9000/profile/names/${username}`,
         dataType: 'json',
