9918635b359af5f1fa5f5a0639e495ab277067fb
Mone't Fulgham <monetfulgham@gmail.com>
2018-02-09 14:16:36 -0500

    Edited for db

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://jmfulgham:em903100@ds225608.mlab.com:25608/routine';
-exports.PORT= process.env.PORT || 9000;
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL ||
+exports.PORT= process.env.PORT || 900
