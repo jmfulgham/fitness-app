a575c01966b1a16977d38acb58265dfa93cf7b5b
Mone't Fulgham <monetfulgham@gmail.com>
2018-02-08 21:11:01 -0500

    Changing host

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://localhost/routine';
+exports.DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL || 'mongodb://jmfulgham:em903100@ds225608.mlab.com:25608/routine';
 exports.PORT= process.env.PORT || 9000;
